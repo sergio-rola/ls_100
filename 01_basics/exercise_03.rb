b428b1ca81c997313183c009007cdd93c5720e25
S.Rola <roladasilva@gmail.com>
2019-04-23 00:06:49 +0800

    prep course - basics

---
+++ 01_basics/exercise_03.rb
@@ -0,0 +1,6 @@
+a = {:sharknado => "1975", :euro => "2004", :not_odisseia => "2013", :odisseia => "2001", :retro => "1981"}
+puts a[:sharknado ]
+puts a[:euro]
+puts a[:not_odisseia]
+puts a[:odisseia]
+puts a[:retro]
